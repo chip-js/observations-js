65b9fc7faa50b092224c48e5654c39fc2cfa8e68
Jacob Wright <jacwright@gmail.com>
2016-04-09 23:13:40 -0600

    Rename load to async

--- src/computed-properties/async.js
+++ src/computed-properties/async.js
@@ -3,19 +3,20 @@ var ComputedProperty = require('./computed-property');
 var expressions = require('expressions-js');
 
 /**
- * Calls the load expression and assigns the results to the object's property when the `whenExpression` changes value to
- * anything other than a falsey value such as undefined. The return value of the load expression should be a Promise.
- * @param {String} whenExpression The conditional expression use to determine when to call the `loadExpression`
- * @param {String} loadExpression The expression which will be executed when the `when` value changes and the result of
+ * Calls the async expression and assigns the results to the object's property when the `whenExpression` changes value
+ * to anything other than a falsey value such as undefined. The return value of the async expression should be a
+ * Promise.
+ * @param {String} whenExpression The conditional expression use to determine when to call the `asyncExpression`
+ * @param {String} asyncExpression The expression which will be executed when the `when` value changes and the result of
  * the returned promise is set on the object.
  */
-function AsyncProperty(whenExpression, loadExpression) {
-  if (!loadExpression) {
+function AsyncProperty(whenExpression, asyncExpression) {
+  if (!asyncExpression) {
     this.whenExpression = 'true';
-    this.load = expressions.parse(whenExpression);
+    this.runAsyncMethod = expressions.parse(whenExpression);
   } else {
     this.whenExpression = whenExpression;
-    this.load = expressions.parse(loadExpression);
+    this.runAsyncMethod = expressions.parse(asyncExpression);
   }
   this.observer = null;
 }
@@ -28,7 +29,7 @@ ComputedProperty.extend(AsyncProperty, {
 
     return observations.createObserver(this.whenExpression, function(value) {
       if (value) {
-        var promise = this.load.call(computedObject);
+        var promise = this.runAsyncMethod.call(computedObject);
         if (promise && promise.then) {
           promise.then(function(value) {
             computedObject[propertyName] = value;
