882a5f6685616fe4d3c95d14a65599af5ceff653
Jacob Wright <jacob.wright@riotgames.com>
2016-06-23 13:22:40 -0600

    Fix bug where null cannot be set in computed object

--- src/computed.js
+++ src/computed.js
@@ -43,7 +43,7 @@ exports.create = function(observations) {
         observer = observations.createObserver(expression, function(value) {
           obj[property] = value;
         });
-      } else if (expression.isComputedProperty) {
+      } else if (expression && expression.isComputedProperty) {
         // Add ComputedProperty's observer to the observers and bind if enabled
         observer = expression.addTo(observations, obj, property);
       } else {
